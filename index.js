4c9f76be6ab97f7dc4654bb94e83d7d5399408ce
Carbon_n <82259548+BloCol@users.noreply.github.com>
2025-11-23 16:23:24 +0700

    The Game Card thing

--- index.js
+++ index.js
@@ -1,28 +1,57 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const swiper = new Swiper(".multiple-slide-carousel", {
-    loop: true,
-    slidesPerView: 3,
-    spaceBetween: 20,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    breakpoints: {
-      1920: {
-        slidesPerView: 3,
-        spaceBetween: 30,
-      },
-      1028: {
-        slidesPerView: 2,
-        spaceBetween: 30,
-      },
-      990: {
-        slidesPerView: 1,
-        spaceBetween: 0,
-      },
-    },
-  });
-});
-console.log('Hello World!')
+const total = 5;
+const gamesTitle = document.getElementById('games-title')
+const gamesInfo1 = document.getElementById('games-info-1')
+let current = 0;
 
 
+function nextSlide()
+{
+  current = current === total ? 0 : current + 1;
+  document.getElementById(`slide${current}`).scrollIntoView({ behavior: 'smooth' });
+  setTitle()
+  setInfo()
+}
+
+function prevSlide() 
+{
+  current = current === 0 ? total : current - 1;
+  document.getElementById(`slide${current}`).scrollIntoView({ behavior: 'smooth' });
+  setTitle()
+  setInfo()
+}
+
+function setTitle()
+{
+  gamesTitle.innerHTML = gameTitles[current]
+}
+
+function setInfo()
+{
+  gamesInfo1.innerHTML = gameInfo[current]
+}
+
+let gameTitles = [
+  "Half-Life",
+  "Counter Strike",
+  "Team Fortress",
+  "Portal",
+  "Left 4 Dead",
+  "Dota"
+]
+
+let gameInfo = [
+  "<span class=\"red\">Half-Life</span> is a series of first-person shooter games that combine shooting combat, puzzles and storytelling, and are played entirely from the first-person perspective.<span class=\"highlight\">The original Half-Life</span> is also <span class=\"red\">Valve</span>'s first product and was released in   for Windows.",
+
+  "<span class=\"red\">Counter Strike</span> is multiplayer tactical first-person shooter in which opposing teams attempt to complete various objectives The series began on Windows in 1999 with the release of the first game, Counter-Strike. ",
+  
+  "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse sunt consectetur voluptatum deserunt voluptas ex est laboriosam modi repellat iste quod ducimus deleniti excepturi explicabo, accusamus doloribus ipsa earum aspernatur!",
+
+  "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse sunt consectetur voluptatum deserunt voluptas ex est laboriosam modi repellat iste quod ducimus deleniti excepturi explicabo, accusamus doloribus ipsa earum aspernatur!",
+
+  "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse sunt consectetur voluptatum deserunt voluptas ex est laboriosam modi repellat iste quod ducimus deleniti excepturi explicabo, accusamus doloribus ipsa earum aspernatur!",
+  
+  "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse sunt consectetur voluptatum deserunt voluptas ex est laboriosam modi repellat iste quod ducimus deleniti excepturi explicabo, accusamus doloribus ipsa earum aspernatur!"
+]
+
+setTitle()
+setInfo()
